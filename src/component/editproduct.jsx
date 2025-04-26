37c35e7e2d1b19d7bbde2b862e92ff8ef7805e0d
Steven-Thomas07 <166240039+Steven-Thomas07@users.noreply.github.com>
2025-04-26 15:16:19 +0530

    Update editproduct.jsx

--- src/component/editproduct.jsx
+++ src/component/editproduct.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import { toast } from 'react-toastify';
-import './AddProduct.css';
+import './Addproduct.css';
 
 function EditProduct() {
   const { id } = useParams();
